43f0bb2c79bc563e59ccd449ed0881903ebcbdd7
Arpan Dey <arpandey.web@gmail.com>
2026-01-29 17:55:56 +0600

    implemented register auth using better auth api

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from "express"
 import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "../lib/auth";
 const app: Application = express()
 const port = process.env.PORT || 8000;
 
@@ -8,7 +10,8 @@ app.use(express.json())
 
 
 
-
+// register user route 
+app.all('/api/auth/*splat', toNodeHandler(auth)); 
 
 app.get("/", (req: Request, res: Response) => {
     res.send(`SkillBridge App Running on PORT: ${port}`)
